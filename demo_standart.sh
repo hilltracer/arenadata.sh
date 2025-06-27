271ba328db4da9ce677792a1ba34450a54cc3769
Denis Garsh <d.garsh@arenadata.io>
2025-06-27 18:37:31 +0300

    update

--- demo_standart.sh
+++ demo_standart.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash -ex
 
 (
 cd /home/gpadmin
@@ -7,8 +7,27 @@ source gpdb_src/concourse/scripts/common.bash
 install_and_configure_gpdb
 gpdb_src/concourse/scripts/setup_gpadmin_user.bash
 make_cluster
-su - gpadmin
-source /usr/local/greenplum-db-devel/greenplum_path.sh;
-source gpdb_src/gpAux/gpdemo/gpdemo-env.sh;
-createdb --owner="$USER" "$USER"
+su - gpadmin -c '
+  source /usr/local/greenplum-db-devel/greenplum_path.sh
+  source gpdb_src/gpAux/gpdemo/gpdemo-env.sh
+  createdb --owner="$USER" "$USER"
+'
+
+
+if [ ! -f /home/gpadmin/.bashrc ]; then
+  cp /etc/skel/.bashrc /home/gpadmin/.bashrc
+fi
+
+cat << 'EOF' >> /home/gpadmin/.bashrc
+
+if [ -f /usr/local/greenplum-db-devel/greenplum_path.sh ]; then
+  source /usr/local/greenplum-db-devel/greenplum_path.sh
+fi
+
+if [ -f ~/gpdb_src/gpAux/gpdemo/gpdemo-env.sh ]; then
+  source ~/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
+fi
+EOF
+chown gpadmin:gpadmin /home/gpadmin/.bashrc
+
 ) 2>&1 | tee "$HOME/demo_standart.log"
