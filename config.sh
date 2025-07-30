9f60f96beae8125a89d08eedda6de9097c6fa72d
Denis Garsh <d.garsh@arenadata.io>
2025-07-30 16:55:08 +0300

    up

--- config.sh
+++ config.sh
@@ -55,5 +55,5 @@ export CXXFLAGS="-DGPOS_DEBUG -O0 -ggdb -g3 -fno-omit-frame-pointer -Wclobbered"
     --with-pythonsrc-ext \
     --with-uuid=e2fs \
     $CONFIGURE_FLAGS
-) 2>&1 | tee "$HOME/config.log"
+) 2>&1 | tee "$HOME/logs/config.log"
 #    --with-wal-segsize=1 \
